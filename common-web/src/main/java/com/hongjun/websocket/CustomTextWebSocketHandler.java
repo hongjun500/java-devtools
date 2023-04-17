1f52a89fc884d53df3a517ab0a6bffbc225e8fc7
hongjun500 <bhj.hong@qq.com>
2023-04-17 20:10:14 +0800

    自定义websocket

---
+++ common-web/src/main/java/com/hongjun/websocket/CustomTextWebSocketHandler.java
@@ -0,0 +1,51 @@
+package com.hongjun.websocket;/**
+ * @author hongjun500
+ * @date 2023/3/28 21:15
+ * @tool ThinkPadX1隐士
+ * Created with 2022.2.2.IntelliJ IDEA
+ * Description:
+ */
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author hongjun500
+ * @date 2023/3/28 21:15
+ * @tool ThinkPadX1隐士
+ * Created with 2019.3.2.IntelliJ IDEA
+ * Description:
+ */
+@Log4j2
+@Component
+public class CustomTextWebSocketHandler extends TextWebSocketHandler {
+
+    private List<WebSocketSession> webSocketSessionList = new ArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(@NonNull WebSocketSession webSocketSession) throws Exception {
+        webSocketSessionList.add(webSocketSession);
+    }
+
+
+    @Override
+    public void afterConnectionClosed(@NonNull WebSocketSession session, @NonNull CloseStatus status) throws Exception {
+        log.info("websocket连接关闭");
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession webSocketSession, TextMessage message) throws Exception {
+        // 接收的消息
+        String messagePayload = message.getPayload();
+        log.info("接收到websocket的消息{}", messagePayload);
+        webSocketSession.sendMessage(new TextMessage(messagePayload));
+    }
+}
