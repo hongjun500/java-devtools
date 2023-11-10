35e33e34d2d36d8bada64c7ff8d4703b3d1e928b
hongjun500 <bhj.hong@qq.com>
2023-11-10 18:17:44 +0800

    gradle setting

---
+++ common-starter/src/test/java/StartTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author hongjun500
+ * @date 2023/11/10 18:08
+ * @tool ThinkPadX1隐士
+ * Created with 2022.2.2.IntelliJ IDEA
+ * Description:
+ */
+
+
+public class StartTest {
+	@Test
+	void test() {
+		System.out.println("Hello World!");
+	}
+}
