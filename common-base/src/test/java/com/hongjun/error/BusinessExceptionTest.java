20f1c60425e0a9556b7a314fa3035c75bce31831
hongjun500 <bhj.hong@qq.com>
2023-11-15 21:44:40 +0800

    自定义异常处理的异常消息修复

--- common-base/src/test/java/com/hongjun/error/BusinessExceptionTest.java
+++ common-base/src/test/java/com/hongjun/error/BusinessExceptionTest.java
@@ -2,13 +2,14 @@ package com.hongjun.error;
 
 import com.hongjun.enums.EnumBusinessError;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @Log4j2
 class BusinessExceptionTest {
 
     @Test
-    // @Disabled
+    @Disabled
     void assertBusinessException() throws BusinessException {
         log.info("test-assertBusinessException------------------");
         log.info("hello world");
@@ -17,7 +18,7 @@ class BusinessExceptionTest {
     }
 
     @Test
-    // @Disabled
+    @Disabled
     void testAssertBusinessException() throws BusinessException {
         BusinessException.assertBusinessException(true, EnumBusinessError.UNKNOWN_ERROR, "custom error message");
     }
